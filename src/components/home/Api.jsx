582ccb8062435077f1c74699eed05450cbc55e05
Velmurugan <velmurukanmani@users.noreply.github.com>
2024-03-29 13:22:54 +0530

    api url changes

--- src/components/home/Api.jsx
+++ src/components/home/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   timeout: 5000,
-  baseURL: "https://vhospitalplus.com/", // Replace with your API URL
+  baseURL: "https://api.vhospitalplus.com/", // Replace with your API URL
   headers: {
     "Content-Type": "application/json",
     accept: "application/json", // If you receieve JSON response.
